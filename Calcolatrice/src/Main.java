6e03349170bf5ffee0754580156dac3f28a2facc
Bruno <bruno1999@hotmail.it>
2023-09-29 10:47:04 +0200

    add input da tastiera

--- Calcolatrice/src/Main.java
+++ Calcolatrice/src/Main.java
@@ -1,12 +1,23 @@
+import java.util.* ;
+
 public class Main {
     public static void main(String[] args) {
-        double valUno = 3;
-        double valDue = 1;
-        char oper = '*';
+        Scanner in = new Scanner(System.in); // input da tastiera
 
-        whatOper(valUno, valDue, oper);
+        System.out.println("Dammi il primo valore: ");
+        double valUno = in.nextInt();    // input da tastiera
+        System.out.println("Scrivere il simbolo dell'operazione da eseguire : ");
+        char oper = in.next().charAt(0);  // input da tastiera
+        System.out.println("Dammi il secondo valore : ");
+        double valDue = in.nextInt();   // input da tastiera
+
+
+
+
+        whatOper(valUno, valDue, oper); //richiamo del metodo per capire tipo di operazione
     }
-    public static void whatOper(double x, double y, char oper){
+
+    public static void whatOper(double x, double y, char oper){ //metodo per capire quale operazione
         switch (oper){
             case '+' :
                 break;
