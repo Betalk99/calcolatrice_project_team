4d223c578e1416abef17132227aa994dfe97ff0e
Bruno <bruno1999@hotmail.it>
2023-09-29 09:50:26 +0200

    add main and feature whatOperation

---
+++ Calcolatrice/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        double valUno = 3;
+        double valDue = 1;
+        char oper = '*';
+
+        whatOper(valUno, valDue, oper);
+    }
+    public static void whatOper(double x, double y, char oper){
+        switch (oper){
+            case '+' :
+                break;
+            case '-' :
+                break;
+            case '*' :
+                break;
+            case '/' :
+                break;
+            default:
+                System.out.println("Operazione errata!");
+                break;
+        }
+    }
+
+
+
+}
