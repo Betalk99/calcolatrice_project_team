7f6b9a51feb599fe78537b46b132780871931325
AB <antoniobuonanno0605@gmail.com>
2023-10-02 10:34:25 +0200

    Buonanno's commit
    This commit adds following classes: Multiplication/Division, and also the update of Class Operazione

--- Calcolatrice/src/Operazione.java
+++ Calcolatrice/src/Operazione.java
@@ -7,8 +7,11 @@ public class Operazione {
             case '-' :
                 break;
             case '*' :
+                double result=Multiplication.moltiplicazione(x,y);
+                System.out.println("Il risultato è: "+result);
                 break;
             case '/' :
+                Division.divisione(x,y);
                 break;
             default:
                 System.out.println("Operazione errata!");
